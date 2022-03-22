2d8d7d19bba7927583be892e8376fda956d4e7c1
saimantmg01 <saimantmg01@gmail.com>
2022-03-21 20:01:19 -0400

    Corrected code after review for prime numbers

--- Team 8/generate_perfect_num.py
+++ Team 8/generate_perfect_num.py
@@ -14,26 +14,36 @@ eg: 6 = 1+2+3
 
     Open questions
     Perfect numbers so far tends to be only even numbers so far. Odd perfect numbers have still not proven yet. 
+
+
+Errors introduced and found:
+- factors including itself. (correction: Should only be proper factors)
+- factors can't be 0.
+- number shouldn't be started at 0. (should start from 1)
+- limit_num shouldn't have 1 added to it. 
 """
 
+
+"""Corrected format"""
 def generate_perfect(limit_num):
     """
     Function description:
     Generate perfect numbers upto user specified limit.
-    @param gets a integer as input which is limit number upto which you want to generate prime numbers
+    @params gets a integer as input which is limit number upto which you want to generate prime numbers
     @returns array of integers where each element is a perfect number. 
     """
     ans = []
-    num = 0
-    while num != (limit_num+1):
+    num = 1
+    while num != limit_num:
         temp_sum = 0
-        for factors in range(0, num):
+        for factors in range(1, num):
             if num % factors == 0:
                 temp_sum += factors
         if temp_sum == num:
             ans.append(num)
         num += 1
     return ans
+
 
 
 if __name__ == "__main__":
